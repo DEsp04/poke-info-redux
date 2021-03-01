009b830e6babbd37e020df14891442e80fcc6e65
david espinal <despinal0425@gmail.com>
2021-02-28 19:17:43 -0500

    added try and catch to the second action creator

--- src/components/PokemonInfo.js
+++ src/components/PokemonInfo.js
@@ -7,23 +7,26 @@ import { fetchPokemonByName } from '../actions';
 
 
 function PokemonInfo(props) {
-  
+  console.log(props)
+
 
   useEffect(() => { 
-    props.fetchPokemonByName();
+    fetchPokemonByName();
   }, [])
 
 
-  const pokemonInfo = props.pokemonInfo.info;
+  // const pokemonInfo = props.pokemonInfo.info;
 
-  if (!pokemonInfo) { 
-    return null;
-  }
+  // if (!pokemonInfo) { 
+  //   return null;
+  // }
 
-  function renderInfo(){ 
+  function renderInfo() { 
+    // if (loading) return <p>Loading Pokémon info...</p>
+    // if (hasErrors) return <p>Unable to display Pokémon</p>
     return (
       <div>
-        <h2>{pokemonInfo.name}</h2>
+        <h2>info.name</h2>
       </div>
     )
   }
@@ -36,8 +39,16 @@ function PokemonInfo(props) {
 }
 
 //each time we select a pokemon, the json keeps adding up to the array in the reducer
-const mapStateToProps = (state) => {
-  return {pokemonInfo: state.pokemonInfo }
-}
+// const mapStateToProps = (state) => {
+//   return {pokemonInfo: state.pokemonInfo }
+// }
+
+
+const mapStateToProps = (state) => ({
+  loading: state.pokemonInfo.loading,
+  info: state.pokemonInfo.info,
+  hasErrors: state.pokemonInfo.hasErrors
+})
+
 
 export default connect(mapStateToProps, { fetchPokemonByName: fetchPokemonByName })(PokemonInfo);
--- src/reducers/pokeInfoReducer.js
+++ src/reducers/pokeInfoReducer.js
@@ -1,12 +1,38 @@
+import * as actions from '../actions';
+
+
+
 
 //because payload will will return an obj from the individual pokemon info, we need to add it to the [] using spread operator
-export default (state = {}, action) => {
+// export default (state = {}, action) => {
 
-  switch(action.type){ 
-    case "FETCH_POKE_INFO":
-      return { ...state, info: action.payload };
+//   switch(action.type){ 
+//     case "FETCH_POKE_INFO":
+//       return { ...state, info: action.payload };
     
+//     default:
+//       return state;
+//   }
+// }
+
+
+
+export const initialState = {
+  info: {},
+  loading: false,
+  hasErrors: false
+}
+
+export default function infoReducer(state = initialState, action) {
+
+  switch (action.type) { 
+    case actions.GET_INFO:
+      return { ...state, loading: true }
+    case actions.GET_INFO_SUCCESS:
+      return { info: action.payload, loading: false, hasErrors: false }
+    case actions.GET_INFO_FAILURE:
+      return { ...state, loading: false, hasErrors: true }
     default:
-      return state;
+      return state
   }
 }
