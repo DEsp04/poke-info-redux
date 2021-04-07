9027cc60f2c4eb5ded97739b661641a93687cdbf
david espinal <despinal0425@gmail.com>
2021-04-07 10:43:44 -0400

    added action and reducers to the slice

--- src/redux/pokemonByUrlSlice.js
+++ src/redux/pokemonByUrlSlice.js
@@ -6,4 +6,26 @@ export const fetchUrl = createAsyncThunk();
 
 
 
-const pokemonUrlSlice = createSlice({})
+const pokemonUrlSlice = createSlice({
+  name: "pokemonUrl",
+  initialState: {
+    pokemonInfo: [],
+    status: null,
+  },
+
+  extraReducers: {
+    [fetchUrl.pending]: (state, action) => {
+      state.status = "loading";
+    },
+    [fetchUrl.fulfilled]: (state, { payload }) => {
+      state.pokemonInfo = payload;
+      state.status = "success";
+    },
+    [fetchUrl.rejected]: (state, actions) => {
+      state.status = "failed";
+    },
+  },
+});
+
+
+export default pokemonUrlSlice.reducer;
