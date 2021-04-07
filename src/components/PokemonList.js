34a64713a83622c9b6c1b63af9ed70097ff7c565
david espinal <despinal0425@gmail.com>
2021-04-07 09:02:57 -0400

    included list of pokemons

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchList } from "../redux/searchPokemonSlice";
-
+import Pokemon from "../components/Pokemon"
 
 export default function PokemonList() {
+
   const pokemonListRequest = useSelector((state) => {
-    return state
+    return state.searches.pokemons
   })
   
   console.log(pokemonListRequest)
@@ -18,11 +19,21 @@ export default function PokemonList() {
   }, [dispatch])
 
 
+  const renderPokemonList = () => {
+    return pokemonListRequest.map((pokemon, index) => {
+      return <Pokemon {...pokemon} key={index} />
+    });
+  }
+
 
   
   return (
     <div>
       <h2>Pokemon List</h2>
+
+      <ul>
+        {renderPokemonList()}
+      </ul>
     </div>
   )
 }
