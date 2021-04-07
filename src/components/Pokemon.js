8442236b2e4a0c7a0bfbe371c29a36e2ec6242ea
david espinal <despinal0425@gmail.com>
2021-04-07 09:23:03 -0400

    added Navlinks to the pokemon nameslist

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from "react-router-dom"
 
 export default function Pokemon(props) {
 
   console.log(props)
   return (
-  
-    <li>{props.name}</li>
+    <NavLink
+      to={{
+        pathname: "/pokemonDescription",
+        state: props
+          ? {
+              name: props.name,
+              pokemonUrl: props.url, 
+            }
+          : {},
+      }}
+    >
+      <li>{props.name}</li>
+    </NavLink>
   )
 }
