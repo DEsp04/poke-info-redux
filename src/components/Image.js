c2ea8c52e5255338e1628aadbbf5c3a815eb44f3
david espinal <despinal0425@gmail.com>
2021-04-09 18:41:50 -0400

    added useSelector for both pokemonInfo and status

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUrl } from "../redux/pokemonByUrlSlice";
 
-export default function Image() {
+
+export default function Image(props) {
+
+  console.log(props)
+  const pokemonUrl = props.url
+
+  const requestPokemonInfo = useSelector((state) => {
+    return state.pokemonInfo
+  })
+  const status = useSelector((state) => {
+    return state.pokemonInfo.status
+  })
+
+
   return (
     
       <p>hello</p>
